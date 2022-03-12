170fba659f5cbbc66dcb3761c4cc70aa1c04bbf1
NamYoung Kim <095201a@naver.com>
2022-03-12 15:16:10 +0900

    play 정적 논리 변수 없애고 break로 대체

--- Main/Main.java
+++ Main/Main.java
@@ -11,15 +11,12 @@ import View.ViewOutput;
 
 public class Main {
 
-	// 계속 플레이 정적 변수
-	private static boolean play = true;
-
 	public static void main(String[] args) throws RspException {
 
 		// Controller 객체 생성
 		Controller controller = new Controller();
 
-		while (play) {
+		while (true) {
 
 			// 게임 시작 문장 출력 메서드 호출
 			ViewOutput.startGame();
@@ -30,20 +27,19 @@ public class Main {
 			controller.playRsp(ViewInput.inputPlayerRsp());
 
 			System.out.println();
-			
+
 			// 컨트롤러에게 가위바위보 판정 요청 졌을 경우 종료
 			if (controller.gameResult() == Result.LOSE) {
 				gameEnd();
+				break;
 			}
 
 		}
 	}
 
 	public static void gameEnd() {
-		System.out.println();
 		ViewOutput.endtitle(); // 끝 문자열 출력
 		ViewOutput.gameResult(); // 게임 결과 출력
-		play = false;
 	}
 
 }
