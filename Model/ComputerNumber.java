8fe3d84ee8c4b7a7f895d19e76fa24ce4b2484fb
NamYoung Kim <095201a@naver.com>
2022-03-12 15:17:25 +0900

    컴퓨터 자동 변수 클래스 분리
    자동 상수 필드 선언

--- Model/ComputerNumber.java
+++ Model/ComputerNumber.java
@@ -3,11 +3,11 @@ package Model;
 
 public class ComputerNumber {
 	
-	// 컴퓨터 자동 숫자 필드 
-	private int AUTO_Number = (int)(Math.random() * 3) + 1;
+	// 컴퓨터 가위바위보용 자동 숫자 상수
+	private final int AUTO_NUMBER = (int)(Math.random() * 3) + 1;
 	
 	// 가위바위보 값 정수형 반환
 	public int getComputerNumber() {
-		return AUTO_Number;
+		return AUTO_NUMBER;
 	}
 }
