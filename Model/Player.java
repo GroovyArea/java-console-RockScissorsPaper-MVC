16d201eb27e1e528ba218372bf5138c2b96444ad
NamYoung Kim <095201a@naver.com>
2022-03-12 15:20:33 +0900

    참가자 필드와, 컴퓨터 인스턴스 getter 추가 및 final 키워드 이용 불변 객체로 변환

--- Model/Player.java
+++ Model/Player.java
@@ -5,18 +5,28 @@ import Enum.Rsp;
 import Exception.RspException;
 
 // 사용자의 정보를 담고 있는 클래스
-// 포함관계를 만들어서 컴퓨터와 사용자의 값을 비교할 것
+// 컴퓨터와 사용자의 값을 비교할 것
 public class Player {
 
 	private final Rsp playerRsp;
 
-	private Computer computer = new Computer();
+	private final Computer computer = new Computer();
 
-	// setter 대신 생성자로 주입 시켜서 해결
+	// setter 대신 생성자로 주입시켜서 해결
 	public Player(final Rsp playerRsp) {
 		this.playerRsp = playerRsp;
 	}
+	
+	// 참가자 가위바위보 반환
+	public Rsp getRsp() {
+		return playerRsp;
+	}
 
+	// 컴퓨터 인스턴스 반환
+	public Computer getComputer() {
+		return computer;
+	}
+	
 	// 게임 결과 반환 메서드
 	public Result play() throws RspException {
 		return Result.getGameResult(playerRsp, computer.getComRsp());
