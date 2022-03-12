c366fee58e7f635d2a379cece4f1af5f87903b2b
NamYoung Kim <095201a@naver.com>
2022-03-12 15:18:46 +0900

    불변 객체로 바꿈
    setter 대신 생성자를 통해 자동상수를 필드에 주입

--- Model/Computer.java
+++ Model/Computer.java
@@ -6,14 +6,16 @@ import Exception.RspException;
 // 컴퓨터의 정보를 담고 있는 클래스
 public class Computer {
 
-	private int comRsp;
-	private ComputerNumber computerNumber = new ComputerNumber();
+	private final int comRsp;
+	private final ComputerNumber computerNumber = new ComputerNumber();
 
-	// setter 대신 생성자로 주입 시켜서 해결
+	// setter 대신 생성자로 대입시켜서 해결
+	// 컴퓨터 인스턴스르 생성 시 자동 숫자(가위바위보) 저장
 	public Computer() {
 		this.comRsp = computerNumber.getComputerNumber();
 	}
 
+	// 컴퓨터 가위바위보를 enum으로 변환하여 반환
 	public Rsp getComRsp() throws RspException {
 		return Rsp.of(comRsp);
 	}
