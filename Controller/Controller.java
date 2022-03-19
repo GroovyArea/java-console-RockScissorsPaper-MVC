7b7000815ec43430dbe2773b83feaba48b7cd6b8
NamYoung Kim <095201a@naver.com>
2022-03-20 00:34:41 +0900

    1. 상태 값 출력 문 메서드화
    2. 컴퓨터 객체 생성을 Controller에서 하고 생성자를 통해 자동번호를 주입

--- Controller/Controller.java
+++ Controller/Controller.java
@@ -5,6 +5,7 @@ import Enum.Result;
 import Enum.Rsp;
 import Exception.RspException;
 import Model.Computer;
+import Model.ComputerNumber;
 import Model.Player;
 import Model.WinDrawCount;
 import View.ViewOutput;
@@ -12,47 +13,49 @@ import View.ViewOutput;
 // 요청을 받는 Controller
 public class Controller {
 
-	// 게임 결과를 가지고 있는 필드
+	// 필드 선언
 	private Result result;
+	private Player player;
+	private Computer computer;
 
 	// play 메서드
-	public void playRsp(Rsp playerRsp, Computer computer) throws RspException {
+	public void playRsp(Rsp playerRsp) throws RspException {
 
-		// 사용자 객체 생성
-		final Player player = new Player(playerRsp);
+		// 참가자 객체 생성
+		player = new Player(playerRsp);
+		// 컴터 객체 생성 및 번호 주입
+		computer = new Computer(new ComputerNumber().getComputerNumber());
 		
 		// 게임결과 반환값을 필드에 저장
 		this.result = gameOf(player, computer);
-		
-		// 가위바위보 상태 값 출력
-		ViewOutput.whatYouInput(player.getRsp(), computer.getComRsp());
-		
-		gameCount(); 
-		resultChange(); 
+
+		rspPrint();
+		gameCount();
+		changedResultPrint();
 	}
 
 	// 승리 무승부 카운트 변환 메서드
 	public void gameCount() {
 		Result.gameCount(result);
 	}
-
-	// viewOutput 결과 바꿔주고 출력하는 메서드
-	public void resultChange() {
-		if (result == Result.WIN)
-			ViewOutput.singleGameResult(PrintGameResult.of(result));
-		if (result == Result.DRAW)
-			ViewOutput.singleGameResult(PrintGameResult.of(result));
-		if (result == Result.LOSE)
-			ViewOutput.singleGameResult(PrintGameResult.of(result));
+	
+	// 가위바위보 상태 값 출력하는 메서드
+	public void rspPrint() throws RspException {
+		ViewOutput.whatYouInput(player.getRsp(), computer.getComRsp());
 	}
-
+	
+	// viewOutput 결과 바꿔주고 출력하는 메서드
+	public void changedResultPrint() {
+		ViewOutput.singleGameResult(PrintGameResult.of(result));
+	}
+	
 	// 결과 얻어오는 메서드
 	public Result gameOf(Player player, Computer computer) throws RspException {
 		return Result.getGameResult(player.getRsp(), computer.getComRsp());
 	}
-	
+
 	// 게임 결과 반환 메서드
-	public Result gameResult() {
+	public Result getGameResult() {
 		return result;
 	}
 }
